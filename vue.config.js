3b24a78d7895576113dc79fd22fcb28f145ffee9
Utsob Roy <roy@utsob.me>
2022-09-27 14:12:03 +0600

    title fix

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "me.utsob.joplinscratchpad",
+        productName: "Joplin Scratchpad",
+      },
+    },
+  },
 });
